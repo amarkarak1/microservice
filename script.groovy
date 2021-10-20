277db6194d4dd6a55fd086d50f98fd5ea2464e04
prajwalyb <32199473+prajwalyb@users.noreply.github.com>
2021-10-20 10:52:28 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,8 @@
 def returncreds(){
 def map = [:]
 map["AWS_ACCOUNT_ID"] = 653709203391
-map["Prajwal"] = "YB"
+map["AWS_DEFAULT_REGION"]="us-east-1" 
+map["IMAGE_REPO_NAME"]="jenkinspipleine"
  
 
 return map
