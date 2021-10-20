04300e1a418a77c13f3daece58639de8d8fad42f
prajwalyb <32199473+prajwalyb@users.noreply.github.com>
2021-10-20 11:00:42 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@ def map = [:]
 map["AWS_ACCOUNT_ID"] = 653709203391
 map["AWS_DEFAULT_REGION"]="us-east-1" 
 map["IMAGE_REPO_NAME"]="jenkinspipleine"
- map["REPOSITORY_URI"] = "${map[AWS_ACCOUNT_ID]}.dkr.ecr.${map[AWS_DEFAULT_REGION]}.amazonaws.com/${map[IMAGE_REPO_NAME]}"
+ map["REPOSITORY_URI"] = "${map["AWS_ACCOUNT_ID"]}.dkr.ecr.${map["AWS_DEFAULT_REGION"]}.amazonaws.com/${map["IMAGE_REPO_NAME"]}"
  
 
 return map
