9a0882b613b97dc993eeccc7a2aa16643b4aa4de
prajwalyb <32199473+prajwalyb@users.noreply.github.com>
2021-10-22 15:53:00 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def returncreds(){
+def map = [:]
+map["AWS_ACCOUNT_ID"] = 653709203391
+map["AWS_DEFAULT_REGION"]="us-east-1" 
+map["IMAGE_REPO_NAME"]="jenkinspipleine"
+ map["REPOSITORY_URI"] = "${map["AWS_ACCOUNT_ID"]}.dkr.ecr.${map["AWS_DEFAULT_REGION"]}.amazonaws.com/${map["IMAGE_REPO_NAME"]}"
+ 
+
+return map
+}
+
+return this
