460efc637b46718e9989da645665647a1e38653b
amar prakash <59972313+amarkarak1@users.noreply.github.com>
2022-05-16 16:12:49 +0530

    Update Test.java

--- src/main/java/javaApp/Test.java
+++ src/main/java/javaApp/Test.java
@@ -2,7 +2,8 @@ package javaApp;
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("Hello!Testing webhook on new instance 6.Congratulations");	
+		System.out.println("Hello!Testing webhook on new instance 6.Congratulations");
+		System.println("Hello!Testing webhook on new instance 6.Congratulations");	
 		
 
 	}
